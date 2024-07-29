84619a3482f23b9295d17d5a059623ed54ab5a20
Baltai Daniel <98697039+BaltaiDaniel@users.noreply.github.com>
2024-07-29 11:23:02 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@
     --------------------*/
     $(window).on('load', function () {
         $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
+        $("#preloder").delay(100).fadeOut("slow");
 
         /*------------------
             Portfolio filter
